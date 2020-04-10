e81c29945ec5608e35b1a5755ab9ac0c0f30531c
TubeXproject <63302023+TubeXproject@users.noreply.github.com>
2020-04-10 09:55:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
 function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("gallery");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.getAttribute("data-title").toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
     } else {
-      li[i].style.display = "none";
+      a[i].style.display = "none";
     }
   }
 }
-myFunction()
